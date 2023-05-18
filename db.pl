5170bd73a9ecfed1c2c7d847273c7556b54618ac
Thiago Vinícius Marcon <thiagovtmarcon@gmail.com>
2023-05-18 12:22:29 +0100

    Create db.pl

---
+++ db.pl
@@ -0,0 +1,82 @@
+Metallica: "Metallica is an American heavy metal band. The band was formed in 1981 in Los Angeles by vocalist and guitarist James Hetfield and drummer Lars Ulrich, and has been based in San Francisco for most of its career."
+
+Albums: 
+
+-----------------------------------------------------------------------------
+Kill 'Em All,  debut (or first) studio album, 1983,  51:20
+
+"Hit the Lights"		 Hetfield, Ulrich			4:17
+
+"The Four Horsemen"	 	 Hetfield, Ulrich, Mustaine		7:13
+
+"Motorbreath"			 Hetfield				3:08
+
+"Jump in the Fire"		 Hetfield, Ulrich, Mustaine		4:42
+
+"(Anesthesia) – Pulling Teeth" 	 Burton				4:15
+
+"Whiplash"			 Hetfield, Ulrich			4:09
+
+"Phantom Lord"			 Hetfield, Ulrich, Mustaine		5:02
+
+"No Remorse"			 Hetfield, Ulrich			6:27
+
+"Seek & Destroy"		 Hetfield, Ulrich			6:56
+
+"Metal Militia"			 Hetfield, Ulrich, Mustaine		5:11
+------------------------------------------------------------------------------
+Ride the Lightning, second studio album, 1984, 47:26
+
+"Fight Fire with Fire"  	Hetfield, Ulrich, Burton 		4:44
+
+"Ride the Lightning"  		Hetfield, Ulrich, Burton, Mustaine 	6:36
+
+"For Whom the Bell Tolls"  	Hetfield, Ulrich, Burton 		5:09
+
+"Fade to Black"  		Hetfield, Ulrich, Burton, Hammett 	6:57
+
+"Trapped Under Ice"  		Hetfield, Ulrich, Hammett 		4:04
+
+"Escape"  			Hetfield, Ulrich, Hammett 		4:24
+
+"Creeping Death"  		Hetfield, Ulrich, Burton, Hammett 	6:36
+
+"The Call of Ktulu" 		Hetfield, Ulrich, Burton, Mustaine 	8:53
+------------------------------------------------------------------------------
+Master of Puppets, third album, 1986, 	54:47
+
+"Battery"   			Hetfield, Ulrich			5:12
+
+"Master of Puppets"   		Hetfield, Ulrich, Burton, Hammett	8:36
+
+"The Thing That Should Not Be"  Hetfield, Ulrich, Hammett 		6:37
+
+"Welcome Home (Sanitarium)"   	Hetfield, Ulrich, Hammett	        6:27
+
+"Disposable Heroes"   		Hetfield, Ulrich, Hammett		8:17
+
+"Leper Messiah"   	    	Hetfield, Ulrich			5:40
+
+"Orion"  			Burton, Hetfield, Ulrich		8:28
+
+"Damage, Inc."   		Hetfield, Ulrich, Burton, Hammett	5:29
+------------------------------------------------------------------------------
+...And Justice For All, fourth album, 1988, 65:33
+    
+"Blackened" 			Hetfield, Ulrich, Newsted 		6:41
+
+"...And Justice for All"	Hetfield, Ulrich, Hammett		9:47 
+
+"Eye of the Beholder" 	 	Hetfield, Ulrich, Hammett	 	6:30
+    
+"One" (Hetfield, Ulrich)	Hetfield, Ulrich			7:26
+    
+"The Shortest Straw" 		Hetfield, Ulrich 			6:36
+    
+"Harvester of Sorrow" 		Hetfield, Ulrich 			5:46
+    
+"The Frayed Ends of Sanity" 	Hetfield, Ulrich, Hammett		7:44
+    
+"To Live Is to Die" 		Hetfield, Ulrich, Burton 		9:49
+    
+"Dyers Eve"			Hetfield, Ulrich, Hammett		5:13		
