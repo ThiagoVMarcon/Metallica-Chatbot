7a7ce8fc33ab31fb388feca8361f84ad366ab0be
strix990 <82136637+strix990@users.noreply.github.com>
2023-05-18 12:25:32 +0100

    Update db.pl

--- db.pl
+++ db.pl
@@ -80,3 +80,139 @@ Master of Puppets, third album, 1986, 	54:47
 "To Live Is to Die" 		Hetfield, Ulrich, Burton 		9:49
     
 "Dyers Eve"			Hetfield, Ulrich, Hammett		5:13		
+
+------------------------------------------------------------------------------
+
+Metallica(The Black album),fifth studio album, 1991, 62:31
+
+1.	"Enter Sandman"  	                    Hetfield, Ulrich, Hammett	5:31
+2.	"Sad but True"  	                    Hetfield, Ulrich	        5:23
+3.	"Holier Than Thou"                      Hetfield, Ulrich	        3:48
+4.	"The Unforgiven"  	                    Hetfield, Ulrich, Hammett	6:27
+5.	"Wherever I May Roam"  	                Hetfield, Ulrich	        6:46
+6.	"Don't Tread on Me"  	                Hetfield, Ulrich	        4:01
+7.	"Through the Never"  	                Hetfield, Ulrich, Hammett	4:03
+8.	"Nothing Else Matters"                  Hetfield, Ulrich	        6:29
+9.	"Of Wolf and Man"  	                    Hetfield, Ulrich, Hammett	4:17
+10.	"The God That Failed"  	                Hetfield, Ulrich	        5:05
+11.	"My Friend of Misery"  	                Newsted, Hetfield, Ulrich	6:50
+12.	"The Struggle Within"  	                Hetfield, Ulrich	        3:54
+Total length: 62:31
+
+------------------------------------------------------------------------------
+
+Load, sixth studio album, 1996, 78:59
+
+1.	"Ain't My Bitch"  	                    Hetfield, Ulrich	        5:04
+2.	"2 X 4"  	                            Hetfield, Ulrich, Hammet	5:28
+3.	"The House Jack Built"  	            Hetfield, Ulrich, Hammet	6:39
+4.	"Until It Sleeps"  	                    Hetfield, Ulrich	        4:30
+5.	"King Nothing"  	                    Hetfield, Ulrich, Hammet	5:28
+6.	"Hero of the Day"  	                    Hetfield, Ulrich, Hammet	4:22
+7.	"Bleeding Me"  	                        Hetfield, Ulrich, Hammet	8:18
+8.	"Cure"  	                            Hetfield, Ulrich	        4:54
+9.	"Poor Twisted Me"  	                    Hetfield, Ulrich	        4:00
+10.	"Wasting My Hate"  	                    Hetfield, Ulrich, Hammet	3:57
+11.	"Mama Said"  	                        Hetfield, Ulrich	        5:19
+12.	"Thorn Within"  	                    Hetfield, Ulrich, Hammet	5:51
+13.	"Ronnie"  	                            Hetfield, Ulrich	        5:17
+14.	"The Outlaw Torn"  	                    Hetfield, Ulrich	        9:52
+Total length:	78:59
+
+------------------------------------------------------------------------------
+
+ReLoad, seventh album, 1997, 1:15:56
+
+1.	"Fuel"  	                            Hetfield, Ulrich, Hammett	4:29
+2.	"The Memory Remains"  	                Hetfield, Ulrich	        4:39
+3.	"Devil's Dance"  	                    Hetfield, Ulrich	        5:18
+4.	"The Unforgiven II"  	                Hetfield, Ulrich, Hammett	6:36
+5.	"Better than You"  	                    Hetfield, Ulrich	        5:21
+6.	"Slither"  	                            Hetfield, Ulrich, Hammett	5:13
+7.	"Carpe Diem Baby"  	                    Hetfield, Ulrich, Hammett	6:12
+8.	"Bad Seed"  	                        Hetfield, Ulrich, Hammett	4:05
+9.	"Where The Wild Things Are"             Hetfield, Ulrich, Newsted	6:52
+10.	"Prince Charming"  	                    Hetfield, Ulrich	        6:04
+11.	"Low Man's Lyric"  	                    Hetfield, Ulrich	        7:36
+12.	"Attitude"  	                        Hetfield, Ulrich	        5:16
+13.	"Fixxxer"  	                            Hetfield, Ulrich, Hammett	8:14
+Total length: 1:15:56
+
+------------------------------------------------------------------------------
+
+St Anger, eighth studio album, 2003, 75:04
+
+All songs in this album were made by James Hetfield, Lars Ulrich, Kirk Hammett and Bob Rock.
+
+1.	"Frantic"	                                                         5:50
+2.	"St. Anger"	                                                         7:21
+3.	"Some Kind of Monster"	                                             8:25
+4.	"Dirty Window"	                                                     5:25
+5.	"Invisible Kid"	                                                     8:30
+6.	"My World"	                                                         5:46
+7.	"Shoot Me Again"	                                                 7:10
+8.	"Sweet Amber"	                                                     5:27
+9.	"The Unnamed Feeling"	                                             7:08
+10.	"Purify"	                                                         5:14
+11.	"All Within My Hands"	                                             8:48
+Total length:	75:04
+
+------------------------------------------------------------------------------
+
+Death Magnetic, ninth studio album, 2008
+
+All lyrics are written by James Hetfield; all music were composed by James Hetfield, Lars Ulrich, Kirk Hammett and Robert Trujillo.
+
+1.	"That Was Just Your Life"	                                         7:08
+2.	"The End of the Line"	                                             7:52
+3.	"Broken, Beat & Scarred"	                                         6:25
+4.	"The Day That Never Comes"	                                         7:56
+5.	"All Nightmare Long"	                                             7:58
+6.	"Cyanide"	                                                         6:40
+7.	"The Unforgiven III"	                                             7:47
+8.	"The Judas Kiss"	                                                 8:01
+9.	"Suicide & Redemption" (instrumental)	                             9:58
+10.	"My Apocalypse"	5:01
+Total length:	74:46
+
+------------------------------------------------------------------------------
+
+Hardwired... to Self-Destruct, tenth studio album, 2016
+
+All tracks are written by James Hetfield and Lars Ulrich, except "ManUNkind", written by Hetfield, Ulrich and Robert Trujillo.
+
+Disc one
+1.	"Hardwired"	                                                          3:09
+2.	"Atlas, Rise!"	                                                      6:28
+3.	"Now That We're Dead"	                                              6:59
+4.	"Moth into Flame"	                                                  5:50
+5.	"Dream No More"	                                                      6:29
+6.	"Halo on Fire"              	                                      8:15
+Disc two
+7.	"Confusion"	                                                          6:43
+8.	"ManUNkind"	                                                          6:55
+9.	"Here Comes Revenge"	                                              7:17
+10.	"Am I Savage?"	                                                      6:30    
+11.	"Murder One"	                                                      5:45
+12.	"Spit Out the Bone"	                                                  7:09
+Total length:	77:42
+
+------------------------------------------------------------------------------
+
+72 seasons, eleventh studio album, 2023
+
+1.	"72 Seasons" 	            James HetfieldLars, UlrichKirk Hammett 	  7:39
+2.	"Shadows Follow"            Hetfield, Ulrich 	                      6:12
+3.	"Screaming Suicide"         Hetfield, Ulrich, Robert Trujillo	      5:30
+4.	"Sleepwalk My Life Away"    Hetfield, Ulrich, Trujillo	              6:56
+5.	"You Must Burn!"            Hetfield, Ulrich, Trujillo	              7:03
+6.	"Lux Æterna"                Hetfield, Ulrich	                      3:22
+7.	"Crown of Barbed Wire"      Hetfield, Ulrich, Hammett	              5:49
+8.	"Chasing Light"             Hetfield, Ulrich, Hammett	              6:45
+9.	"If Darkness Had a Son"     Hetfield, Ulrich, Hammett	              6:36
+10.	"Too Far Gone?"             Hetfield, Ulrich	                      4:34
+11.	"Room of Mirrors"           Hetfield, Ulrich	                      5:34
+12.	"Inamorata"                 Hetfield, Ulrich	                      11:10
+Total length: 77:10
+
+
