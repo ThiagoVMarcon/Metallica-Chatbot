9d2be3985aad17f0ea4ae67a279484f12b56724a
strix990 <82136637+strix990@users.noreply.github.com>
2023-05-20 18:31:05 +0100

    Update execteste.pl
    Implementei o a versap teste do pattern matching

--- execteste.pl
+++ execteste.pl
@@ -1,26 +1,52 @@
-imp :- write("Escreva sua mensagem(nao esqueca de abrir e fechar aspas) "),read(X), read_split(X). % pega input do usuario
+imp :- write("Escreva sua mensagem(nao esqueca de abrir e fechar aspas) "),read(X), read_split(X). % pega imput do usuario
 
-adicionar_espaco([], []).     % funcao para adicionar espaços em branco no output
-adicionar_espaco([X|Resto], [NovoX|NovoResto]) :-
-    string_concat(X, " ", NovoX),
+caracter_especial('.').
+caracter_especial(',').
+caracter_especial('!').
+caracter_especial('?').
+caracter_especial(';').
+caracter_especial(':').
+
+remover_caracter_especial([], []).
+remover_caracter_especial([X|Resto], NovoResto) :-
+    caracter_especial(X),
+    remover_caracter_especial(Resto, NovoResto).
+remover_caracter_especial([X|Resto], [X|NovoResto]) :-
+    \+ caracter_especial(X),
+    remover_caracter_especial(Resto, NovoResto).
+
+remover_caracteres_especiais(Imps, Outs) :-
+    string_chars(Imps, Listachar),
+    remover_caracter_especial(Listachar, Listachar_2),
+    string_chars(Outs, Listachar_2).
+
+adicionar_espaco([], []).
+adicionar_espaco([X|Resto], [X2|NovoResto]) :-
+    string_concat(X, " ", X2),
     adicionar_espaco(Resto, NovoResto).
 
-process_Words([], []).  % processamento de palavras, muda uma palavra pre-definida por outra
+process_Words([], []).
 process_Words([H|T], [H2|T2]) :-
     translate(H, H2),
     process_Words(T, T2).
 
 translate("ola", "tchau"):-!.
 translate("bem", "mal"):-!.
-translate(X, X). % catch-all clause for all words not to be translated % padroes de mudança
+translate(X, X). % catch-all clause for all words not to be translated
 
-process_Pattern([],[]). % in work
-process_Pattern() 
+process_Pattern(["what","is","your","name"|_], ["Hey","my","name","is","Robert","I'm","a","chatbot","that","really","loves","Metalica!"]).
+process_Pattern(["What","is","your","favorite","album"|_],["idk","lol"]).
+process_Pattern(["What","is","your","favorite","album?"|_],["idk","lol"]).
+process_Pattern([_|Resto],Y) :- process_Pattern(Resto,Y).
+process_Pattern([],["Sorry","couldn't","quite","catch","you","man"]).
 
-read_split("stop") :- !. 
-read_split(X) :- split_string(X, "\s", "\s", Out),temp(Out), imp. % transforma o input em uma lista de palavras separadas por espaços
+read_split("stop") :- !.
+read_split(X) :- remover_caracteres_especiais(X, Outs),split_string(Outs, "\s", "\s", Out),temp(Out), imp. % transforma o input em uma lista de palavras separadas por espaços
 
-temp(X) :- process_Words(X,Y),adicionar_espaco(Y,Z),atomics_to_string(Z, O), write(O),write("\n"). % faz o output, a funcao atomics_to_string transforma uma lista de strings em uma string
+temp(X) :- process_Pattern(X,Y),adicionar_espaco(Y,Z),atomics_to_string(Z, O), write(O),write("\n").
 
+teste :- write("pao").
+
+testers :- split_string(" computer consists of hardware ", "\s", "\s", L), flatten(L,L2),write(L2).
 
 
