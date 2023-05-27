9d76896b5561014b7cafcff9084af99c1a38d14c
strix990 <82136637+strix990@users.noreply.github.com>
2023-05-27 23:00:07 +0100

    Update input_process.pl

--- input_process.pl
+++ input_process.pl
@@ -10,7 +10,7 @@ imp :- write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(X,
 is_composed_response([X, "is pretty cool! Have you ever heard of", Y], R) :- write("Cliff: "), adicionar_espaco([X, "is pretty cool! Have you ever heard of", Y],Z),atomics_to_string(Z, O), write(O),write("\n"), write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(Sla, Ascii_imp), read_split(Sla, 1, Y, R).
 is_composed_response([X, "is pretty bad... Have you ever heard of", Y], R) :- write("Cliff: "), adicionar_espaco([X, "is pretty bad... Have you ever heard of", Y],Z),atomics_to_string(Z, O), write(O),write("\n"), write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(Sla, Ascii_imp), read_split(Sla, 1, Y, R).
 is_composed_response(["My name is Cliff, I'm a chatbot that really loves Metallica! How about you, what's your name?"], R) :- write("Cliff: "), adicionar_espaco(["My name is Cliff, I'm a chatbot that really loves Metallica! How about you, what's your name?"],Z),atomics_to_string(Z, O), write(O),write("\n"), write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(Sla, Ascii_imp), read_split(Sla, 2, [], R).
-is_composed_response(["Im doing good, how about you?"], R) :- write("Cliff: "), adicionar_espaco(["Im doing good, how about you?"],Z),atomics_to_string(Z, O), write(O),write("\n"), write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(Sla, Ascii_imp), read_split(Sla, 3, [], R).
+is_composed_response(["I`m doing good, how about you?"], R) :- write("Cliff: "), adicionar_espaco(["I`m doing good, how about you?"],Z),atomics_to_string(Z, O), write(O),write("\n"), write("User: "),read_line_to_codes(user_input, Ascii_imp),string_codes(Sla, Ascii_imp), read_split(Sla, 3, [], R).
 is_composed_response(X,X).
 
 get_age(X,A) :- artist(X,A,_).
@@ -75,26 +75,28 @@ process_Words([H|T], [H2|T2]) :-
     translate(H, H2),
     process_Words(T, T2).
 
+translate("ola", "tchau"):-!.
+translate("bem", "mal"):-!.
 translate(X, X). % catch-all clause for all words not to be translated
 
 throw_away_rest_besides_last([X], X).
 throw_away_rest_besides_last([_|X], R) :- throw_away_rest_besides_last(X, R).
 
-process_Pattern(["yes"|_], 1, X, ["Cool! I really love", X, "."]).
-process_Pattern(["no"|_], 1, _, ["You should try it out! It's really good, I swear."]).
-process_Pattern(["bad"], 3, _, ["Hope things get better!"]).
-process_Pattern([X|Y], 2, _,["Nice to meet you", R,"!"]) :- is_name_introduction(X), throw_away_rest_besides_last(Y,R).
+process_Pattern(["yes"|_], 1, X, ["Cool! I really love", X]).
+process_Pattern(["no"|_], 1, _, ["You should try it out! It's really good I swear"]).
+process_Pattern([X|Y], 2, _ ,["Nice to meet you", R,"!"]) :- is_name_introduction(X), throw_away_rest_besides_last(Y,R).
+process_Pattern(["good"], 3, _, ["Good to hear!"]).
+process_Pattern(["bad"], 3, _, ["Hope things get better"]).
 process_Pattern(X,_,_,Y) :- process_Pattern(X,Y).
 process_Pattern([X], ["Hey!"]) :- is_greeting(X).
-process_Pattern(["how", "are", "you"], ["I'm doing good, how about you?"]).
 process_Pattern(["are","you","called","cliff"], ["Yep!"]).
 process_Pattern(["is","your","name","cliff"], ["Yep!"]).
-process_Pattern(["I am good"], ["Good to hear!"]).
 process_Pattern([X, "cliff"],["Hey there!"]) :- is_greeting(X).
 process_Pattern(["i'm", X, "what","is","your","name"|_],["Hey", X, "My name is Cliff, I'm a chatbot that really loves Metallica!"]).
 process_Pattern(["i'm","called",X, "what","is","your","name"|_],["Hey", X, "My name is Cliff, I'm a chatbot that really loves Metallica!"]).
 process_Pattern(["i","am", X, "what","is","your","name"|_],["Hey", X, "My name is Cliff, I'm a chatbot that really loves Metallica!"]).
 process_Pattern(["i","am","called",X, "what","is","your","name"|_],["Hey", X, "My name is Cliff, I'm a chatbot that really loves Metallica!"]).
+process_Pattern(["how", "are", "you"], ["I`m doing good, how about you?"]).
 process_Pattern(["like",X|_],["Do you mean the album or the song? Either case I think they are both pretty great!"]) :- is_same_name_of_album(X).
 process_Pattern(["like", "the", X|_],Y) :- compare_music2(X,X2),random_between(1,2,R), responses(R, X2, Y).
 process_Pattern(["like", X|_],Y) :- compare_music(X,X2),random_between(1,2,R), responses(R, X2, Y).
@@ -102,8 +104,8 @@ process_Pattern(["like", X|_],Y) :- compare_album(X,X2),random_between(3,4,R), r
 process_Pattern(["what","is","your","name"|_], ["My name is Cliff, I'm a chatbot that really loves Metallica! How about you, what's your name?"]).
 process_Pattern(["what","year",_,"metallica"|_], ["Metallica was created in 1981, in Los Angeles."]).
 process_Pattern(["what","year",_,"the","band"|_], ["Metallica was formed in 1981, in Los Angeles."]).
-process_Pattern(["what","is","your","favorite","album"|_],["My favorite album is Master of Puppets but I really like", X, "too!"]) :- random_between(1,2,A), album(A,X,_,_,_). % albus favoritos
-process_Pattern(["what","is","your","favorite", "music"|_], ["My favorite music is Fade to Black but I really like", X , "aswell."]) :- random_between(1,5,R1),random_between(1,7,R2),album(R1,A,_,_,_),music(R2,X,A,_,_).
+process_Pattern(["what","is","your","favorite","album"|_],["My favorite album is Master of Puppets but I really like", X, "too"]) :- random_between(1,2,A), album(A,X,_,_,_). % albus favoritos
+process_Pattern(["what","is","your","favorite", "music"|_], ["My favorite music is Fade to Black but I really like", X , "aswell"]) :- random_between(1,5,R1),random_between(1,7,R2),album(R1,A,_,_,_),music(R2,X,A,_,_).
 process_Pattern(["how","many", "albums"|_], ["Metallica has released a total of 11 studio albums."]). % quantos albuns
 process_Pattern(["what","year","is",X|_], [Y,"is","from",Z,"!"]) :- compare_album(X,Y), album(_,Y,_,Z,_).% ano de lançamentos do album
 process_Pattern(["who","is", "the", "vocalist"|_], ["James Hetfield is the current and only vocalist that Metallica has ever had."]). % vocalista
@@ -123,7 +125,7 @@ process_Pattern(["what","is","the","longest", "music"|_],["It","is","Inamorata",
 process_Pattern(["what","is","the","longest","album"|_],["It","is","Load","which","has","The Outlaw Torn","as","its","longest","song"]).% albuns mais longos
 process_Pattern(["what","is","the","longest","album"|_],["It","is","Load","which","has","The Outlaw Torn","as","its","longest","song"]).% albuns mais longos
 process_Pattern(["what","do","you","think","about","metallica", "after","black"|_],["Metallica created a whole genre of music, Thrash Metal, and then walked away from it in 1990 because of greed. Some ppl say to me get over it blah blah blah but music to me is after family and friends. I loath anyone who turns their back on a legion of fans and a genre of music and walk away because of greed. They were not poor when they released the black album, they were just greedy. Sad."]). % voce acha que o metalica se vendeu no black album, oque voce acha do black album
-process_Pattern([],["Sorry","couldn't","quite","catch","you","man."]).
+process_Pattern([],["Sorry","couldn't","quite","catch","you","man"]).
 process_Pattern([_|Resto],Y) :- process_Pattern(Resto,Y).
 
 read_split("stop") :- !.
@@ -162,6 +164,7 @@ name_introduction("people").
 name_introduction("is").
 % name_introduction(["is","what","people","call","me"]).
 % name_introduction(["is","what","people","like","to","call","me"]).
+name_introduction(X).
 name_introduction([]).
 
 responses(1, X, [X, "is pretty cool! Have you ever heard of", Y]) :- random_between(1,5,R1),random_between(3,8,R2),album(R1,A,_,_,_),music(R2,Y,A,_,_).
